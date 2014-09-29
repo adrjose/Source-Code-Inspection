690facf691f20f0523c1308303b99a64c2eda290
santos-7 <santos-7@BR01L-E6430-06.am.mt.mtnet>
2014-09-28 21:29:57 -0300

    Este é meu commit após inserir a classe meuTeste.java

--- Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
@@ -49,4 +49,8 @@ public class TicketMachine {
         result += "*****************\n";
         return result;
     }
+    public String testeGithub()
+            {
+                return "Meu Teste no Github";
+            }
 }
